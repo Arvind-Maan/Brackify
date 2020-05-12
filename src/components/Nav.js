ab0dc53cf940d8f797f8f32ddec8ea0da7b37570
Arvind Maan <arvindmaan1199@gmail.com>
2020-05-11 20:28:43 -0500

    added initial components

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+	return (
+		<nav className="nav-container">
+			<div className="nav-wrapper">
+				<a href="#" className="logo">
+					{"{"}Brackify{"}"}
+				</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
---
+++ src/components/Workspace.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Workspace = () => {
+    return (
+        <div className="workspace"> 
+        </div>
+    );
+};
+export default Workspace;
